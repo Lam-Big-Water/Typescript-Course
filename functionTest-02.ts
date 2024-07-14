1124b776dcd307827f74263a59745062f916390b
Sam Lam <xlin67948gmail.com>
2024-07-14 13:09:29 +0800

    Challenge-01

---
+++ functionTest-02.ts
@@ -0,0 +1,7 @@
+import addNumbers, {addStrings, format, introduce, getName} from "./basicFunction-02";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings('1'));
+console.log(format('a', 1));
+console.log(introduce('a', 'b', 'c', 'd'));
+console.log(getName());
--- functionTest.ts
+++
@@ -1,7 +0,0 @@
-import addNumbers, {addStrings, format, introduce, getName} from "./basicFunction";
-
-console.log(addNumbers(1, 2));
-console.log(addStrings('1'));
-console.log(format('a', 1));
-console.log(introduce('a', 'b', 'c', 'd'));
-console.log(getName());
